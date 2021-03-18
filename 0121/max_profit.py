0d4788aad5428321bd1e8f94173d66cdb103fcba
Wendell Sun <iwendellsun@gmail.com>
2021-03-18 20:57:30 +0800

    fixup! 0318 update

--- 0121/max_profit.py
+++ 0121/max_profit.py
@@ -6,3 +6,19 @@ class Solution:
         pass
 
     def maxProfit(self, prices: List[int]) -> int:
+        result, lowest = 0, 0
+        for i in range(1, len(prices), 1):
+            if prices[i] < prices[lowest]:
+                lowest = i
+            else:
+                result = max(result, prices[i] - prices[lowest])
+        return result
+
+
+def main():
+    s = Solution()
+    print(s.maxProfit([7, 1, 5, 3, 6, 4]))
+
+
+if __name__ == "__main__":
+    main()
