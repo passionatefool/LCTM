84ebcb0e471c79ecc41c7bf149f5ec11ee32bd94
Wendell Sun <iwendellsun@gmail.com>
2021-04-14 02:00:28 +0800

    Add 0035 python version

---
+++ 0035/search_insert.py
@@ -0,0 +1,6 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i, num in enumerate(nums):
+            if target <= num:
+                return i
+        return len(nums)
