7f2acde098339ddee28250de3140206b40b4e1a9
lesywix <lin.wx@foxmail.com>
2021-10-13 23:22:06 +0800

    Add 1143 go version

---
+++ algorithms/1143/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	dp := make([][]int, len(text1)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(text2)+1)
+	}
+	for i := 1; i <= len(text1); i++ {
+		for j := 1; j <= len(text2); j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[len(text1)][len(text2)]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println(longestCommonSubsequence("abcde", "ace"))
+}
